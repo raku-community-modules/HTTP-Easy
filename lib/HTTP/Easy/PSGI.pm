4ac7097e7ada0fb9e2c156f3f7bb4390e7160594
Timothy Totten <2010@huri.net>
2011-09-15 21:35:36 -0700

    Fixed up some issues, now works, but nom needs buffer flushing.

--- lib/HTTP/Easy/PSGI.pm
+++ lib/HTTP/Easy/PSGI.pm
@@ -14,30 +14,30 @@ method app ($app) {
 
 method handler {
   ## First, let's add any necessary PSGI variables.
-  %!env<psgi.version>      = [1,0];
-  %!env<psgi.url_scheme>   = 'http'; ## TODO: detect this.
-  %!env<psgi.multithread>  = False;
-  %!env<psgi.multiprocess> = False;
-  %!env<psgi.input>        = $!body;
-  %!env<psgi.errors>       = $*ERR; ## TODO: allow override on this.
-  %!env<psgi.run_once>     = False;
-  %!env<psgi.nonblocking>  = False; ## Allow when NBIO.
-  %!env<psgi.streaming>    = False; ## Eventually?
+  %.env<psgi.version>      = [1,0];
+  %.env<psgi.url_scheme>   = 'http'; ## TODO: detect this.
+  %.env<psgi.multithread>  = False;
+  %.env<psgi.multiprocess> = False;
+  %.env<psgi.input>        = $.body;
+  %.env<psgi.errors>       = $*ERR; ## TODO: allow override on this.
+  %.env<psgi.run_once>     = False;
+  %.env<psgi.nonblocking>  = False; ## Allow when NBIO.
+  %.env<psgi.streaming>    = False; ## Eventually?
   my $result;
   if $!app ~~ Callable {
-    $result = $!app(%!env);
+    $result = $!app(%.env);
   }
   elsif $!app.can('handle') {
-    $result = $!app.handle(%!env);
+    $result = $!app.handle(%.env);
   }
   else {
     die "Invalid {self.WHAT} application.";
   }
-  my $output = %!env<SERVER_PROTOCOL>~' '~$result[0]~$!crlf;
+  my $output = %.env<SERVER_PROTOCOL>~' '~$result[0]~$!crlf;
   for @($result[1]) -> $header {
     $output ~= $header.key ~ ': ' ~ $header.value ~ $!crlf;
   }
-  my $body = $return[2].join($!crlf);
+  my $body = $result[2].join($!crlf);
   $output ~= $!crlf ~ $body;
   return $output;
 }
